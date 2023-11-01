6abd4c6bf59cf0486abb02a9175fabaabefa3c3f
mattyboiii <mattripodi.6@gmail.com>
2023-11-01 06:01:34 +0000

    Updating some pages.

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,17 @@
+<div>
+    
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="save Item">save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete Item">Delete</button>
+    </form>
+
+</div>
